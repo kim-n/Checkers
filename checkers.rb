0533ea39abda1907f36f6713bd91d5a6fa29712b
Kimberly Narine <kimberlyonarine@gmail.com>
2013-10-31 16:10:53 -0400

    Remove kings sliding multiple spots

--- checkers.rb
+++ checkers.rb
@@ -1,4 +1,4 @@
-class InvalidMoveEror < StandardError
+class InvalidMoveError < StandardError
 end
 
 class Board
@@ -7,35 +7,6 @@ class Board
   def initialize
     @grid = Array.new(8){ Array.new(8) {nil} }
     addpawns
-  end
-
-  def piece_at(pos)
-    @grid[pos[0]][pos[1]] unless @grid[pos[0]][pos[1]].nil?
-  end
-
-  def move!(s_pos, e_pos)
-    start_x, start_y = s_pos[0], s_pos[1]
-    end_x, end_y = e_pos[0], e_pos[1]
-
-    #Removes stray pointers
-    @grid[end_x][end_y].board = nil if !@grid[end_x][end_y].nil?
-
-    @grid[end_x][end_y] = @grid[start_x][start_y]
-    @grid[end_x][end_y].position = e_pos if !@grid[end_x][end_y].nil?
-    @grid[start_x][start_y] = nil
-  end
-
-  def print_board
-    8.times do |i|
-      8.times do |j|
-        if @grid[i][j].is_a?(Pawn)
-          print "#{@grid[i][j].color} "
-        else
-          print "- "
-        end
-      end
-      print "\n"
-    end
   end
 
   def addpawns
@@ -52,6 +23,79 @@ class Board
       end
     end
   end
+
+  def piece_at(pos)
+    @grid[pos[0]][pos[1]] unless @grid[pos[0]][pos[1]].nil?
+  end
+
+  def slide!(s_pos, e_pos)
+    start_x, start_y = s_pos[0], s_pos[1]
+    end_x, end_y = e_pos[0], e_pos[1]
+
+    @grid[end_x][end_y] = @grid[start_x][start_y]
+    @grid[end_x][end_y].position = e_pos
+    @grid[start_x][start_y] = nil
+  end
+
+  def find_piece_to_remove(s_pos, e_pos)
+    x = (s_pos[0] - e_pos[0])/2
+    y = (s_pos[1] - e_pos[1])/2
+    [e_pos[0] + x, e_pos[1] + y ]
+  end
+
+  def jump!(s_pos, e_pos)
+    mid_piece = find_piece_to_remove(s_pos, e_pos)
+
+
+    start_x, start_y = s_pos[0], s_pos[1]
+    end_x, end_y = e_pos[0], e_pos[1]
+
+    @grid[end_x][end_y] = @grid[start_x][start_y]
+    @grid[end_x][end_y].position = e_pos
+    @grid[start_x][start_y] = nil
+    @grid[mid_piece[0]][mid_piece[1]] = nil
+
+  end
+
+  def print_board
+    8.times do |i|
+      8.times do |j|
+        if @grid[i][j].is_a?(Pawn)
+          print "#{@grid[i][j].color} "
+        else
+          print "- "
+        end
+      end
+      print "\n"
+    end
+  end
+
+  def move!(s_pos, e_pos)
+     start_x, start_y = s_pos[0], s_pos[1]
+     end_x, end_y = e_pos[0], e_pos[1]
+
+     #Removes stray pointers
+     @grid[end_x][end_y].board = nil if !@grid[end_x][end_y].nil?
+
+     @grid[end_x][end_y] = @grid[start_x][start_y]
+     @grid[end_x][end_y].position = e_pos if !@grid[end_x][end_y].nil?
+     @grid[start_x][start_y] = nil
+   end
+
+   # def perform_moves
+   #   if move_sequence.size > 1
+   #     begin
+   #       perform_moves!(*move_sequence)
+   #     #perform jump
+   #   else
+   #     #perform slide sequence
+   #   end
+   #
+   #   # checks validmove_seq?
+   #   # calls perform_moves! or raises InvalidMoveError
+   # end
+
+
 
 end  #END BOARD
 
@@ -72,9 +116,10 @@ class Pawn
     @king = king
   end
 
+
   def offsets
     offsets = []
-    if self.king != true
+    unless self.king
       self.color == :w ? offsets = Diagonals[0..1] : offsets = Diagonals[2..3]
     else
       offsets = Diagonals
@@ -82,50 +127,13 @@ class Pawn
     offsets
   end
 
-
   def is_valid?(pos)
     pos.all? {|d| d.between?(0,7)}
   end
 
   def occupied?(pos,color = nil)
     piece_at_pos = @board.piece_at(pos)  # nil if there is no piece there
-    if color.nil?
-      piece_at_pos
-    elsif !piece_at_pos.nil?
-      piece_at_pos && (piece_at_pos).color == color
-    else
-      nil
-    end
-
-  end
-
-
-  def pawn_jump_moves
-    possible_moves = []
-    offsets.each do |offset|
-      dx, dy = @position[0] + offset[0], @position[1]+ offset[1]
-      if is_valid?([dx,dy]) && occupied?([dx,dy])
-        possible_moves += [dx,dy] if !occupied?([dx+offset[0],dy+ offset[1]])
-      end
-    end
-
-    possible_moves
-
-
-    # elsif occupied?([dx,dy], @color) #occupied by me
-    #   break
-    # else  #occupied by next color
-    #   #check that position after is empty
-    #   dx, dy = dx + offset[0], dy + offset[1]
-    #   next unless is_valid?([dx,dy])
-    #   if occupied?([dx,dy]) # by anyone
-    #     break
-    #   else  #blank spot
-    #     possible_moves << [dx,dy]
-    #     dx, dy = dx + offset[0], dy + offset[1]
-    #   end
-    # end
-  end
+    color.nil? ? piece_at_pos : (piece_at_pos && (piece_at_pos).color == color)  end
 
 
   def slide_moves
@@ -134,12 +142,7 @@ class Pawn
 
     offsets.each do |offset|
       dx, dy = @position[0] + offset[0], @position[1]+ offset[1]
-      while is_valid?([dx,dy])
-        break if occupied?([dx,dy])
-        possible_moves << [dx, dy]
-        break if @king == false
-        dx, dy = dx + offset[0], dy + offset[1]
-      end
+      possible_moves << [dx, dy] if is_valid?([dx,dy]) && !occupied?([dx,dy])
     end
     possible_moves
   end
@@ -148,38 +151,42 @@ class Pawn
     offsets = self.offsets
     possible_moves = []
 
-    jump_sequence(@position)
-  end
-
-  def jump_sequence(pos)
-    offsets = self.offsets
-
-    possible_moves = {}
-
     offsets.each do |offset|
-      dx, dy = pos[0], pos[1]
-      moves = Hash.new{[]}
+      dx, dy = @position[0], @position[1]
+      dx2, dy2 = dx + offset[0], dy + offset[1] #next spot
+      dx3, dy3 = dx2 + offset[0], dy2 + offset[1] #next next spot
 
-      while(is_valid?([dx,dy]))
-        dx2, dy2 = dx + offset[0], dy + offset[1] #next spot
-        dx3, dy3 = dx2 + offset[0], dy2 + offset[1] #next next spot
-
-        break if ( !is_valid?([dx3,dy3]) || occupied?([dx3,dy3]) ||   occupied?([dx2,dy2], @color) )
-        moves[[dx,dy]] += [[dx3,dy3]]
-        dx, dy = dx3 , dy3
-        # jump_sequence([dx,dy]) if @king == true
-      end
-      possible_moves.merge!(moves){|key, oldval, newval| newval + oldval}
+      next if ( !is_valid?([dx3,dy3]) || occupied?([dx3,dy3]) ||  !occupied?([dx2,dy2]) || occupied?([dx2,dy2], @color) )
+      possible_moves << [dx3,dy3]
     end
     possible_moves
+
   end
 
-  def perform_slide
+
+  def perform_slide(start_pos, end_pos)
+    if @board.piece_at(start_pos).nil?
+      raise InvalidMoveError, "Nothing at start position"
+    elsif !self.slide_moves.include?(end_pos)
+      raise InvalidMoveError, "Cant slide to that position"
+    end
+
+    @board.slide!(start_pos, end_pos)
     # validate the move
     # illegal slide should raise an InvalidMoveError.
   end
 
-  def perform_jump
+
+  def perform_jump(start_pos, end_pos)
+    if @board.piece_at(start_pos).nil?
+      raise InvalidMoveError, "Nothing at start"
+    elsif !self.jump_moves.include?(end_pos)
+      p self.jump_moves
+      raise InvalidMoveError, "Can't jump to end"
+    end
+
+    @board.jump!(start_pos, end_pos)    # Perform jump
+    #@board.jump!(@position, end_pos)
     # validate the move
     # remove the jumped piece from the
     # illegal jump should raise an InvalidMoveError.
@@ -192,12 +199,20 @@ class Pawn
     #
   end
 
-  def perform_moves
-    # checks validmove_seq?
-    # calls perform_moves! or raises InvalidMoveError
-  end
 
-  def perform_moves!(move_sequence)
+
+
+  def perform_moves!(*move_sequence)
+    moves = move_sequence
+    if moves.size > 1
+      #perform jumps
+      (moves.size-1).times do |t|
+        perform_jump(moves[t], moves[t+1])
+      end
+    else
+
+      #perform slide sequence
+    end
     # that takes a sequence of moves
     # one slide or
     # one or more jumps.
@@ -209,24 +224,74 @@ class Pawn
 
 end
 
+#________T_E_S_T_S_________#
+
 game = Board.new
-game.print_board
+
 pawn = game.grid[2][0]
-p pawn.slide_moves()
 
+game.move!([5,1], [3,1])
 
+game.print_board
 pawn.king = true
 
- #game.move!([2,0], [4,2])
- game.move!([6,4], [3,1])
- game.move!([2,0], [6,4])
 
- pawn = game.grid[6][4]
- game.print_board
+puts
+game.print_board
 
-# #p game.piece_at([1,1]).color
- #pawn = game.grid[4][2]
+ begin
+ pawn.perform_moves!(pawn.position, [4,2])
 
+ #pawn.perform_jump(pawn.position, [4,2])
+rescue InvalidMoveError => e
+  puts e
 
- pawn.slide_moves()  # slidedoes not take care of nil
+end
+
+puts
+game.print_board
 p pawn.jump_moves()
+p pawn.slide_moves()
+#
+#
+#  #game.move!([2,0], [4,2])
+#  #
+#  #game.move!([2,0], [6,4])
+#
+#  #pawn = game.grid[6][4]
+#  pawn.king = true
+#
+#  # p "Slide from [2,0] to [4,2]"
+#  # pawn.perform_slide(pawn.position, [4,2])
+#  # game.print_board
+#
+#  puts
+#  game.move!([6,4], [3,1])
+#  # game.print_board
+#
+#  #
+#  # puts
+#  # p "Jump from [4,2] to [6,4]"
+#  # pawn.perform_jump(pawn.position, [4,2])
+#  # game.print_board
+# #
+#  begin
+#  pawn.perform_moves!(pawn.position, [4,2], [6,4], [4,6])
+#
+#  #pawn.perform_jump(pawn.position, [4,2])
+# rescue InvalidMoveError => e
+#   puts e
+#
+# end
+#
+# puts
+# game.print_board
+#
+# p pawn.jump_moves
+# p pawn.slide_moves()
+# # #p game.piece_at([1,1]).color
+#  #pawn = game.grid[4][2]
+#
+#
+# # p pawn.slide_moves()  # slidedoes not take care of nil
+# # p pawn.jump_moves()
